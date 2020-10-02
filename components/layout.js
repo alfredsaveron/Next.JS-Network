f247f49ba400c78a686ba021e835199ea208e148
Kaguwo <60431344+kaguwomin@users.noreply.github.com>
2020-10-02 22:47:33 +0300

    Add files via upload

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import { children } from 'react'
+import Navbar from './navbar'
+
+function Layout ({children}) {
+    return (
+        <div>
+            <Navbar/>
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout;
